e053b51c4d40c7f1b1fbc403f5f7a03762ede378
Arnaud <arnaud.delannoy752@gmail.com>
2022-08-22 18:45:31 +0200

    création dépot + Création dossiers CSS & JS + insertion 3 fichiers HTML

---
+++ JS/index.js
@@ -0,0 +1,59 @@
+let mainTitle=document.getElementById("MainTitle")
+// Élément 1 = Fonction Flag (utile pour écrans publicitaires par exemple):
+// function Ads(_flag)
+// {
+//     if(_flag==0)
+//     {
+//         mainTitle.innerHTML="renaud"
+//         _flag=1
+//     }
+//     else
+//     {
+//         mainTitle.innerHTML="Bonjour monsieur."
+//         _flag=0
+//     }
+//     setTimeout(() => Ads(_flag), timeout=10000);
+// }
+// Ads(1)
+// Élément 2 = Modif du titre dans le html + changement de titre selon un délai demandé:
+// mainTitle.innerHTML="Renaud"
+// let userInput=parseInt(prompt("Tapez 1 si vous êtes un homme, ou 2 si vous êtes une femme."))
+// if(userInput==1)
+// {
+//     mainTitle.innerHTML="Bonjour monsieur."
+// }
+// else
+// {
+//     mainTitle.innerHTML="Bonjour madame."
+// }
+// setTimeout(() =>
+// {
+//     mainTitle.innerHTML="Renaud"
+//  }, 10000);
+
+// Exercice: Mix des deux éléments + passage d'une couleur à l'autre:
+mainTitle.innerHTML="Renaud"
+let userInput=parseInt(prompt("Tapez 1 si vous êtes un homme, ou 2 si vous êtes une femme."))
+if(userInput==1)
+{
+     mainTitle.innerHTML="Bonjour monsieur."
+}
+else if(userInput==2)
+{
+    mainTitle.innerHTML="Bonjour madame."
+}
+else
+{
+ alert("Désolé, la valeur introduite ne correspond pas à ce qui est demandé.")
+}
+setTimeout(() =>
+{
+    mainTitle.innerHTML="Renaud"
+ }, 10000);
+ let MyButton=document.getElementById("MonBouton")
+ MyButton.addEventListener("click",function()
+ {
+    alert("Hello World!")
+ }
+ )
+ MyButton.addEventListener("mouseover",function(){alert("Bonnes vacances!")})
